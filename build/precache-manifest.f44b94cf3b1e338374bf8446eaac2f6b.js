2a63ff43e3129f009ed448431e95b78f770bf47f
Haneenabonser <aabonser@gmail.com>
2021-08-18 05:41:13 +0300

    fix

---
+++ build/precache-manifest.f44b94cf3b1e338374bf8446eaac2f6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48bdd1d10172c960f66175b734da9421",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd87bcac2e539c860a25",
+    "url": "/static/css/2.cd554934.chunk.css"
+  },
+  {
+    "revision": "272d3b02b51d59f1aa64",
+    "url": "/static/css/main.02a68802.chunk.css"
+  },
+  {
+    "revision": "bd87bcac2e539c860a25",
+    "url": "/static/js/2.33e28aa5.chunk.js"
+  },
+  {
+    "revision": "c1bdf306936ddd82567a345834b1f9d5",
+    "url": "/static/js/2.33e28aa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "272d3b02b51d59f1aa64",
+    "url": "/static/js/main.de9bb9b4.chunk.js"
+  },
+  {
+    "revision": "8d17fc892ec6f45ced5d",
+    "url": "/static/js/runtime-main.4c3a264e.js"
+  }
+]);
